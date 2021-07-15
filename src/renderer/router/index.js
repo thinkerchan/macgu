b227e5b0179a4cb659473db86d137ac495f06f4e
chenxinkai <chenxinkai@pconline.com.cn>
2021-07-15 17:30:11 +0800

    update

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 封装一个 _import, 便于splitChunks
+const _import = require('./_import_' + process.env.NODE_ENV)
+
+// 如果不需要splitChunks, 用同步组件
+// const _import = require("./_import_development");
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'index',
+      component: _import('index'),
     },
     {
       path: '*',
---
+++ src/renderer/views/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-index">
+    咪咕音乐桌面客户端
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'index',
+    components:{
+
+    },
+    data(){
+      return {
+
+      }
+    },
+    mounted(){
+      console.log(location.href);
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style lang="scss">
+.page-index{
+
+}
+</style>
